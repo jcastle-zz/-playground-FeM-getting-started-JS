c811f4766dd625ea83029765276b36db9942dd02
jcastle <joseph.castle@gsa.gov>
2020-06-03 11:02:57 -0400

    adding notes and files

--- exercises/three-pillars/ex.js
+++ exercises/three-pillars/ex.js
@@ -1,3 +1,28 @@
+/*
+FeM Getting Started w/ JS
+
+# Three Pillars of JS
+This is an exercise to briefly practice the three pillars of JS: Types / Coercion, Scope / Closures, and `this` / Prototypes.
+
+## Instructions
+
+1. In the `printFavoriteBooks()` function, make sure there's no accidental type conversion (ie, from number to string).
+	- Hint: Use `String(..)` to coerce something to a string type.
+
+2. Move the `addFavoriteBook(..)` and `printFavoriteBooks()` functions into the `Bookshelf` class as methods. Modify them so they use the `this` keyword to access the `favoriteBooks` array.
+	- Hint: `class` methods don't use the `function` keyword, just their name.
+
+3. Fill out the definition of the `loadBooks(..)` function, which should receive an instance of the `Bookshelf` class that you will pass to it.
+
+4. `loadBooks(..)` should call the provided `fakeAjax(..)`, using `BOOK_API` as the URL and an inline function expression as the callback.
+
+5. The callback will be passed an array of book names. Loop through this array, passing each book name to the `addFavoriteBook(..)` method of the `Bookshelf` instance passed to `loadBooks(..)`. Then call the `printFavoriteBooks()` method.
+
+6. Create an instance of `Bookshelf` class, and pass it as an argument to `loadBooks(..)`.
+	- Hint: Class instantiation: `new Whatever()`.
+*/
+
+
 class Bookshelf {
 	constructor() {
 		this.favoriteBooks = [];
@@ -5,26 +30,33 @@ class Bookshelf {
 
 	// TODO: define methods `addFavoriteBook(..)`
 	// and `printFavoriteBooks()`
-}
 
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
+	addFavoriteBook(bookName) {
+		if (!bookName.includes("Great")) {
+			this.favoriteBooks.push(bookName);
+		}
+	}
+
+	printFavoriteBooks() {
+		console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+		for (let bookName of this.favoriteBooks) {
+			console.log(bookName);
+		}
 	}
 }
 
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
-
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
+function loadBooks(theBookShelf) {
+	fakeAjax(BOOK_API, function onBooks(bookNames) {
+		for (let bookName of bookNames) {
+			theBookShelf.addFavoriteBook(bookName);
+		}
+		theBookShelf.printFavoriteBooks();
+	});
 }
 
 var BOOK_API = "https://some.url/api";
+var myBooks = new Bookshelf();
+loadBooks(myBooks);
 
 
 // ***********************
--- playground/notes.js
+++ playground/notes.js
@@ -96,3 +96,44 @@ function timeRemaining(timeElapsed, endTime) {
 }
 var left = timeRemaining(42, 240);
 console.log(left);
+
+/*
+Three Pillars of JS.
+1. Types/coercion
+    - Primitive types
+        - "In JS, everything is an object" - not true! because we have primitive types
+        - undefined, string, number, boolean, object, symbol, null (behaves strangely, don't get back what we expect)
+        - In JS, variables don't have types, values do
+    * Special values 
+        - NaN - most think it is "not a number" but not necessarily true, indicates invalid numeric value
+        - new - used with object(), array(), function(), date(), RegExp(), error() - instantiate new instances. Do not use new with string(), number(), and boolean().
+    - Converting types 
+        - Going from number to a string and back. One type of implicit conversion is concatenation (number to string).
+        - Booleans - falsy and truthy - means which values when converted to boolean become true or false.
+            - Falsy - "", 0, -0, null, NaN, false, undefined.
+            - Truthy - "string", 23, {a:1}, [1:3], true, function(){}. Infinitely long list of true values. Basically falsy then everything else.
+        - Coercion best practices - explicit vs implicit coercion. "A quality JS program always embraces coercion, making sure the types involved in every operation are clear." Make it obvious if doing math, everything is a number. 
+    - Checking equality - checking values. 
+        - == vs. === 
+            - == - allows coercion, used when types are different.
+            - === - disallows coercion, used when types are the same.
+            - When types are the same, the double and triple equals do the same thing.
+            - Only when types are different, do double and triple equals have different behavior.
+    - Is the coercion helpful? Is it helpful to me or not. 
+        - == - is not really about comparisons of unknown types. Should make types obvious when making comparisons. If you know the types then you can decide when to use == equals.
+        - JS has a dynamic system to use coercion.
+2. Scope
+    - Nested scope
+        - Scope - where in the program to look for things.
+        * Special values - undefined and undeclared. Distinctly different, undefined has no value and undeclared was never declared in the program.
+        - Function expression - function that is assigned as a value. "First class citizen." A value that can be passed around.
+        - IIFEs - Immediately Invoked Function Expression - Function executed immediately after function expression is described as a value. Creates a new block of scope.
+        - Block scoping - Instead of an IIFE, use {} with let. AKA encapsulation.
+    - Closure 
+        - Have to understand scope before we can understand closure.
+        - Closure is when a function remembers variables outside of it even if you pass that function elsewhere.
+3. this/prototypes
+    - this - this keyword references an execution context for when function is called. Determined entirely by how function was called. This-aware function can have a different context each time it is called. This is used in dynamic context. 
+    - prototypes - need to understand this keyword to use prototypes. More common now to use class.
+    - class{} - layered on top of prototype system. Looks more like class design in C++ or Java. 
+*/
